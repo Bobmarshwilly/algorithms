b7873ec5e6a447aae5044af1cdc03faf035471bd
Bobmarshwilly <danilaloginov5@gmail.com>
2025-03-22 02:16:23 +0300

    Delete Algorithms directory

--- Algorithms/binary_search.py
+++
@@ -1,6 +0,0 @@
-from algorithms import SearchAlgorithms as s
-
-lst = [i for i in range(1000)]  # отсортированный список
-
-binary_search_result = s.BinarySearch.search(lst, 49)  # Поиск числа 49 в списке lst
-print(binary_search_result)
